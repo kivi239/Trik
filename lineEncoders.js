d2b501c1b377633bf82daa061a939d8f05595cb9
kivi239 <ilya.kuzmina@yandex.ru>
2015-05-07 19:13:27 +0300

    Right signs

--- lineEncoders.js
+++ lineEncoders.js
@@ -1,29 +1,31 @@
 var __interpretation_started_timestamp__;
-var k = 0,5;
-var k1 = 2;
+var k = 0.5;
+var k1 = 0;
 var v = 50;
 
 var main = function()
 {
-	__interpretation_started_timestamp__ = Date.now();
+  __interpretation_started_timestamp__ = Date.now();
 
-  brick.lineSensor("video1").init(true);
+  var sens = brick.lineSensor("video0");
+  sens.init(true);
   while (!brick.keys().wasPressed(KeysEnum.Enter)) {
-  	script.wait(100);
+    script.wait(100);
   }
-
- 	var oldx = 0;
+  sens.detect();
+	
+  var oldx = 0;
 
   while (true) {
-  	left = brick.encoder(B3).readRawData();
-  	right = brick.encoder(B4).readRawData();
-  	x = brick.lineSensor("video1")
+    left = brick.encoder(B3).readRawData();
+    right = brick.encoder(B4).readRawData();
+    x = sens.read()[0];
 	
-		u = k * x + k1 * (x - oldx);
-		brick.motor(M3).setPower(v + u);
-		brick.motor(M4).setPower(v - u);
-		oldx = x;
-		script.wait(30);
+    u = k * x + k1 * (x - oldx);
+    brick.motor(M3).setPower(v + u);
+    brick.motor(M4).setPower(v - u);
+    oldx = x;
+    script.wait(30);
 
-	}
+  }
 }
