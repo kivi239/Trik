0cc71328b548b006bb9ddd048bf6daabfdaccff6
kivi239 <ilya.kuzmina@yandex.ru>
2015-05-11 17:38:31 +0300

    Fix with new motors

--- forwardAndBack.js
+++ forwardAndBack.js
@@ -10,38 +10,38 @@ var main = function()
   var encL = [];
   var encR = [];
   
-  var e3 = brick.encoder("B2");
-  var e4 = brick.encoder("B1");
-  e3.reset();
-  e4.reset();
+  var rightEnc = brick.encoder("B2");
+  var leftEnc = brick.encoder("B1");
+  rightEnc.reset();
+  leftEnc.reset();
 
-  var m3 = brick.motor("M2");
-  var m4 = brick.motor("M1");
-  m3.setPower(50);
-  m4.setPower(50);
+  var rightM = brick.motor("M2");
+  var leftM = brick.motor("M1");
+  leftM.setPower(50);
+  rightM.setPower(50);
   
   var time = 0;
   var l = 0;
   var r = 0;
   while (time < 1600) {
-    l = e3.readRawData();
-    r = e4.readRawData();
+    l = leftEnc.readRawData();
+    r = rightEnc.readRawData();
     print(l + " " + r);
     encL.push(l);
     encR.push(r);
     var diff = l + r;
-    m3.setPower(50 + k * diff);
-    m4.setPower(50 - k * diff);
+    rightM.setPower(50 + k * diff);
+    leftM.setPower(50 - k * diff);
     script.wait(20);
     time += 20;
   }
   print("here");
-  while (l <= 0 && r >= 0) {
-    l = e3.readRawData();
-    r = e4.readRawData();
+  while (l >= 0 && r <= 0) {
+    r = rightEnc.readRawData();
+    l = leftEnc.readRawData();
     var diff = l + r;
-    m3.setPower(-50 + k * diff);
-    m4.setPower(-50 - k * diff);
+    rightM.setPower(-50 + k * diff);
+    leftM.setPower(-50 - k * diff);
     script.wait(20);
   }
 }
--- lineEncoders.js
+++ lineEncoders.js
@@ -1,6 +1,6 @@
 var __interpretation_started_timestamp__;
 var k = 0.5;
-var k1 = 1.5;
+var k1 = 0;//1.5;
 var v = 50;
 
 var main = function()
@@ -14,22 +14,36 @@ var main = function()
   }
   sens.detect();
 	
-  var oldx = 0;
-  var e3 = brick.encoder("B3");
-  var e4 = brick.encoder("B4");
+  var oldx = sens.read()[0];
+  var e3 = brick.encoder("B1");
+  var e4 = brick.encoder("B2");
   e3.reset();
   e4.reset();
 
+  var leftM = brick.motor(M1);
+	var rightM = brick.motor(M2);
+
   while (true) {
-    left = brick.encoder(B3).readRawData();
-    right = brick.encoder(B4).readRawData();
+    left = e3.readRawData();
+    right = e4.readRawData();
     x = sens.read()[0];
-	
+    print(x + " : " + oldx)	
     u = k * x + k1 * (x - oldx);
-    brick.motor(M3).setPower(v + u);
-    brick.motor(M4).setPower(v - u);
+    rightM.setPower(v - u);
+    leftM.setPower(v + u);
     oldx = x;
     script.wait(10);
 
   }
+	
+	var leftM = brick.motor(M1);
+	var rightM = brick.motor(M2);
+	while (true) {
+		u = k * brick.lineSensor("video0").read()[0];
+		rightM.setPower(25 - u);
+		leftM.setPower(25 + u);
+		script.wait(30);
+	}
+
+
 }
